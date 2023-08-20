4489e4e3a2f98a40e5889ab5129893767f19b191
Vivek <veeveckjeet@gmail.com>
2023-08-20 11:49:51 -0400

    Login Signup pages n Routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import Home from "./pages/Home";
 import "./App.css";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+  },
+  {
+    path: "/login",
+    element: <LoginPage></LoginPage>,
+  },
+  {
+    path: "/login",
+    element: <LoginPage></LoginPage>,
+  },
+  {
+    path: "/signup",
+    element: <SignUpPage></SignUpPage>,
+  },
+]);
 function App() {
   return (
     <div className="App">
-      <Home />
+      <RouterProvider router={router} />
     </div>
   );
 }
